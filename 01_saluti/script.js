88098a2dbe46a2f1b7ae8958fec58235977c82ad
SergioGBosco <sergiogibosco@gmail.com>
2025-07-08 15:13:53 +0200

    feat: add function and the call it

---
+++ 01_saluti/script.js
@@ -0,0 +1,17 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+
+// Dichiara la funzione qui.
+function hello(name) {
+  return (`Ciao ${name}`);
+}
+
+
+// Invoca la funzione qui e stampa il risultato in console
+
+hello(userName)
+console.log(hello(userName));
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
