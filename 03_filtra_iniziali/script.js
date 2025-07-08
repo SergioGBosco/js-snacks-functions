ae2fcce015b633bcddb07c58a48dbfc152760255
SergioGBosco <sergiogibosco@gmail.com>
2025-07-08 16:20:24 +0200

    feat: Add a function for check the first letter of array elements to insert them into a variable

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,37 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
+//creo una variabile per permettere all'utente di inserire una lettera 
+
+const startletter = prompt(`Inserisci una lettera`)
 
 // Dichiara la funzione qui.
+function firstletter(array, x) {
 
+
+  //creo una variabile attualemente vuota per permettere al programma di andare ad inserire all'interno i nomi di persona che iniziano con la lettera indicata dall'utente 
+
+  const letterofname = [];
+
+  //creo un ciclo for per andare a ciclare ogni singolo elemento e confrontare la sua inziale con quella scelta dall'utente
+
+  for (let i = 0; i < array.length; i++) {
+    let checkname = array[i];
+
+    //stabilisco la condizione da rispettare, ovvero se la prima lettera del nome da controllare è uguale a quella scelta dall'utente pusho l'elemento controllato in una variabile creata precedentemente.
+
+    if (checkname.charAt(0) === startletter) {
+      letterofname.push(checkname)
+    }
+  }
+
+  //Metto return al di fuori del ciclo for perchè altrimenti si ferma al primo valore positivo trovato 
+
+  return letterofname
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+firstletter(names, startletter)
+console.log(firstletter(names, startletter))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
