7da7f09872cbf7da73a80846a9a6d50d5fc79906
SergioGBosco <sergiogibosco@gmail.com>
2025-07-08 17:58:40 +0200

    feat: use a cicle for to check how many and which vowels are included in the string

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,29 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+//creo una funzione che mi permette di elencare le vocali all'interno dell'elemento 
+function vocaliword(string) {
 
+  //Creo una variabile vuota dove all'interno andrò ad inserire solo le vocali trovate nell'elemento in questione e un altra variabile per confrontare le vocali 
+
+  const vocali = [];
+  const vocalicheck = [`a`, `e`, `i`, `o`, `u`, `A`, `E`, `I`, `O`, `U`];
+  //ciclo gli elementi ottenuti dopo la scomposizione, se questi sono vocali le inserisco nella variabile "vocali" creata in precedenza
+
+  for (let i = 0; i < string.length; i++) {
+    const letter = string[i];
+    //aggiungo la condizione utilizzando l'istruzione includes, che mi permette di confrontare ogni singola lettera con quelle all'interno della variabile creata apposta per il confronto. 
+
+    if (vocalicheck.includes(letter)) {
+      vocali.push(letter);
+    }
+  }
+  return vocali.length + vocali
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+vocaliword(word)
+console.log(vocaliword(word))
 
 
-
-//Risultato atteso se si passa 'javascript': 3 (a, a, i)
+//Risultato atteso se si passa 'javascript': 3 (a, a, i).
