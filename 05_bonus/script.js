94028bc75c76101f48e92fc0fc25a01168cbb6fc
SergioGBosco <sergiogibosco@gmail.com>
2025-07-08 18:22:32 +0200

    feat:  add a function that changes the message depending on the time of day

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -10,9 +10,30 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+//creo una variabile che contiene un saluto
+function hello(string) {
+  let message = `Buon giorno ${string}`;
+
+  //creo una variabile per stabilire l'ora
+  const ora = new Date();
+  const ore = ora.getHours();
+
+  if (ore >= 13 && ore < 17) {
+    message = `Buon Pomeriggio ${string}`;
+  }
+
+  else if (ore >= 17 && ore < 24) {
+    message = `Buona sera ${string}`;
+  }
+
+  return message
+}
+
+
+
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+hello(name);
+console.log(hello(name))
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
